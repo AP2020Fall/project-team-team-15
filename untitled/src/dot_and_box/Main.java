5dfbf0705d55ccc8cb064b37765f0b2c7a404ebd
sadra mns <sadra.mansouri@yahoo.com>
2020-12-04 19:24:19 +0330

    Add constructor

--- untitled/src/dot_and_box/Main.java
+++ untitled/src/dot_and_box/Main.java
@@ -8,4 +8,16 @@ public class Main {
     private String redName, blueName;
     Scanner scanner;
     String[] players = {"Select player","Human"};
+
+    public Main() {
+        System.out.print("Please Enter your names");
+        scanner=new Scanner(System.in);
+        redName=scanner.next();
+        blueName=scanner.next();
+    }
+
+    public static void main(String[] args) {
+        Main main=new Main();
+    }
 }
+
