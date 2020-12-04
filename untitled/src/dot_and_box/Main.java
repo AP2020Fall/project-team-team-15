4af75a438f163a21e675ee0ef5518711fd04248c
sadra mns <sadra.mansouri@yahoo.com>
2020-12-04 19:09:46 +0330

    Update Main

--- untitled/src/dot_and_box/Main.java
+++ untitled/src/dot_and_box/Main.java
@@ -1,4 +1,11 @@
 package dot_and_box;
 
+import java.util.Scanner;
+
 public class Main {
+    private int n;
+    private GameSolver redSolver, blueSolver;
+    private String redName, blueName;
+    Scanner scanner;
+    String[] players = {"Select player","Human"};
 }
