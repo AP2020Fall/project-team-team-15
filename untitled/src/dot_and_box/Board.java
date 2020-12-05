2c405033b79160bda2cb7040391f48c15198da5e
Ramin <Raminfarahani79@yahoo.com>
2020-12-05 18:53:39 +0330

    Hi dear friend: At first I initialize(package dot_and_box;) that shown us the package name of this project;If we dont mention this line the project wouldn't be RUN.Then we import two different package;First we used All the awt in java.Second we import JAVA'S ArrayList.After all these changes we create the class Board.We initialize all final static that we couldn't change them.we use them for the color we want sooner.

---
+++ untitled/src/dot_and_box/Board.java
@@ -0,0 +1,12 @@
+package dot_and_box;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Board {
+
+    final static int RED = 0;
+    final static int BLUE = 1;
+    final static int BLACK = 2;
+    final static int BLANK = 3;
+
+}
