b4a9cf8dae2169e042d6ee5dcbdb41b32a507486
Ramin <Raminfarahani79@yahoo.com>
2020-12-10 18:19:29 +0330

    hello,this is main ,first we import classes then we print hello and create a class of game.we use for to get number of player

--- untitled/src/dot_and_box/Main.java
+++ untitled/src/dot_and_box/Main.java
@@ -1,23 +1,29 @@
 package dot_and_box;
 
 import java.util.Scanner;
+import console.core.Game;
+import console.core.InvalidMoveException;
+import console.core.data.model.ConsolePlayer;
+import console.core.data.model.Player;
+import console.core.data.model.Square;
+import java.util.Scanner;
 
 public class Main {
-    private int n;
-    private GameSolver redSolver, blueSolver;
-    private String redName, blueName;
-    Scanner scanner;
-    String[] players = {"Select player","Human"};
-
-    public Main() {
-        System.out.print("Please Enter your names");
-        scanner=new Scanner(System.in);
-        redName=scanner.next();
-        blueName=scanner.next();
-    }
-
     public static void main(String[] args) {
-        Main main=new Main();
+        Scanner input=new Scanner(System.in);
+        System.out.println("Hello, Welcome to Dots and Boxes");
+        int size=8;
+        Game<Square> game = new Game<>(Square.class, size);
+        int numOfPlayers = 2;
+        for (int i = 0; i < numOfPlayers; i++) {
+            System.out.print("Enter name of " + (i + 1) + "th player: ");
+            String name = input.nextLine();
+            if (name == null || name.length() < 1) {
+                i--;
+                continue;
+            }
+            game.playerJoin(new ConsolePlayer(name));
+        }
     }
 }
 
