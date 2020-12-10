84e20b7b3d7f2da29e289044b209692b0551562d
Ramin <Raminfarahani79@yahoo.com>
2020-12-10 18:40:59 +0330

    hello dear friend first of all,we just import all the java platform we need later. then we create class main, we create scanner to get sth from user,and after that we create game class,in for loop we define number of player , I mean that the game is multiple,then we get the name of user , if the name was null we create one,

--- untitled/src/console/controller/Main.java
+++ untitled/src/console/controller/Main.java
@@ -1,4 +1,29 @@
 package console.controller;
 
+
+import console.core.Game;
+import console.core.InvalidMoveException;
+import console.core.data.model.ConsolePlayer;
+import console.core.data.model.Player;
+import console.core.data.model.Square;
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Hello, Welcome to Dots and Boxes");
+        int size=8;
+        Game<Square> game = new Game<>(Square.class, size);
+        int numOfPlayers = 2;
+        for (int i = 0; i < numOfPlayers; i++) {
+            System.out.print("Enter name of " + (i + 1) + "th player: ");
+            String name = input.nextLine();
+            if (name == null || name.length() < 1) {
+                i--;
+                continue;
+            }
+            game.playerJoin(new ConsolePlayer(name));
+        }
+
 }
+
