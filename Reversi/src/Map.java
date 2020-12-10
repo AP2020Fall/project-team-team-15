ac9a66195abe1bc5934b9a24fe351ab69ec6a116
sadra mns <sadra.mansouri@yahoo.com>
2020-12-10 16:25:25 +0330

    Create loops

--- Reversi/src/Map.java
+++ Reversi/src/Map.java
@@ -1,3 +1,10 @@
 public class Map {
+    public void printMap(){
+        System.out.print("\n  1   2   3   4   5   6   7   8");
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
 
+            }
+        }
+    }
 }
