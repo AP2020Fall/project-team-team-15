52caf39eaf32ad44c8386b36abdf378419451ba0
Ramin <Raminfarahani79@yahoo.com>
2020-12-10 18:52:12 +0330

    In while loop every thing is clear,so I want to explain it: the while loop will continue until the game ended; first of all we show the names of player (I will create the  the class later),then we show you the h|v column with the signichure you should enter (we defined it in other class); now we get the command,in if command we will show you what happened if the user put h ,exit or also h; in every single step we throw an exception too,

--- untitled/src/console/controller/Main.java
+++ untitled/src/console/controller/Main.java
@@ -25,5 +25,35 @@ public class Main {
             game.playerJoin(new ConsolePlayer(name));
         }
 
+        while (!game.isEndGame()) {
+            System.out.println("\nPlayers in order to play:");
+            showPlayers(game);
+            System.out.println("Game board status:");
+            showBoard(game);
+
+            System.out.println(game.getCurrentPlayer() + ": Enter your move in following format:");
+            System.out.println("H | V row column");
+            System.out.println("for example:");
+            System.out.println("H 2 3");
+
+            final String command = input.next();
+            if (command.equalsIgnoreCase("exit"))
+                break;
+
+            if (command.equalsIgnoreCase("h"))
+                try {
+                    game.addHorizontalLine(input.nextInt(), input.nextInt());
+                } catch (InvalidMoveException e) {
+                    System.out.println(e.getMessage());
+                }
+            else
+                try {
+                    game.addVerticalLine(input.nextInt(), input.nextInt());
+                } catch (InvalidMoveException ignored) {
+
+                }
+        }
+    }
+
 }
 
