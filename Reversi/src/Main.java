8e26894ab59d7b2dc39d2d9479b5b008f5f4b881
sadra mns <sadra.mansouri@yahoo.com>
2020-12-10 16:22:03 +0330

    Update Main

--- Reversi/src/Main.java
+++ Reversi/src/Main.java
@@ -1,2 +1,10 @@
 public class Main  {
+    public static int[] nuts = new int[65];
+    public static int player;
+    public static int x;
+    public static int y;
+
+    public static void main(String[] args) {
+
+    }
 }
