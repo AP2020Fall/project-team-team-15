30d9c7b54f2ecc196e091c7e7875af4b839abf8e
Ramin <Raminfarahani79@yahoo.com>
2020-12-11 19:06:02 +0330

    in this section i just do all the thing you mention in the question for example  if nts == 1 we rite if statement just seem what ever the question want.

--- Reversi/src/checkMethods.java
+++ Reversi/src/checkMethods.java
@@ -13,6 +13,36 @@ public class checkMethods {
             int center = btnIndex;
             boolean flag = false;
             //player 1
+            if (Main.nuts[btnIndex] == 1) {
+                while (true) {
+                    if ((cnt == -9 || cnt == 7 || cnt == -1) && location % 8 == 0) {     //out of map
+                        break;
+                    }
+                    if ((cnt == 9 || cnt == -7 || cnt == 1) && location % 8 == 1) {      //out of map
+                        break;
+                    }
+                    if (location - cnt <= 0 || location - cnt > 64) {                    //out of map
+                        break;
+                    }
+                    if (Main.nuts[location - cnt] == 1) {
+                        if (flag) {
+                            while (center != (location - cnt)) {
+                                Main.nuts[center] = 1;
+                                center -= cnt;
+                            }
+                            break;
+                        } else {
+                            break;
+                        }
+                    } else if (Main.nuts[location - cnt] == 2) {
+                        flag = true;
+                    } else {
+                        break;
+                    }
+                    location -= cnt;
+                }
+            }
+            //player 2
             }
     }
 }
