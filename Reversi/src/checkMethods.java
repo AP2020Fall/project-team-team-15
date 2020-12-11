bbacd71733043fcd05581389371cea89294d2fff
Ramin <Raminfarahani79@yahoo.com>
2020-12-11 18:20:28 +0330

    hello dear friend,we create changeColor with initialize value,in this method we want to change the method's color then we initialize the counter value for the next loop I define later

---
+++ Reversi/src/checkMethods.java
@@ -0,0 +1,11 @@
+/**
+ * CheckMethods
+ * have changeColor method to changing color
+ * have CheckMove method to check move
+ */
+
+public class checkMethods {
+    static void changeColor(int start, int finish, int type, int counter, int btnIndex) {
+        int cnt = 0;
+    }
+}
