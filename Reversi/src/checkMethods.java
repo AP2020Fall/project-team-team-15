d2f414ec484baccd6a60894e4c5222ac52b7fd50
Ramin <Raminfarahani79@yahoo.com>
2020-12-12 20:58:33 +0330

    for player 2/we define all location with while true loop & the if statement.if each if statement will true the location decrease 1 from the counter

--- Reversi/src/checkMethods.java
+++ Reversi/src/checkMethods.java
@@ -43,6 +43,34 @@ public class checkMethods {
                 }
             }
             //player 2
+            if (Main.nuts[btnIndex] == 2) {
+                while (true) {
+                    if ((cnt == -9 || cnt == 7 || cnt == -1) && location % 8 == 0) {      //out of map
+                        break;
+                    }
+                    if ((cnt == 9 || cnt == -7 || cnt == 1) && location % 8 == 1) {       //out of map
+                        break;
+                    }
+                    if (location - cnt <= 0 || location - cnt > 64) {                     //out of map
+                        break;
+                    }
+                    if (Main.nuts[location - cnt] == 2) {
+                        if (flag) {
+                            while (center != (location - cnt)) {
+                                Main.nuts[center] = 2;
+                                center -= cnt;
+                            }
+                            break;
+                        } else {
+                            break;
+                        }
+                    } else if (Main.nuts[location - cnt] == 1) {
+                        flag = true;
+                    } else {
+                        break;
+                    }
+                    location -= cnt;
+                }
             }
     }
 }
