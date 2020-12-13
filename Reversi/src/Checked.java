a402374953c7e7d5581a6b56249094a258b22eef
sadra mns <sadra.mansouri@yahoo.com>
2020-12-13 12:45:32 +0330

    Update Checked

--- Reversi/src/Checked.java
+++ Reversi/src/Checked.java
@@ -10,6 +10,20 @@ public class Checked {
         CheckMethods.checkMove(-1, 2, 0, 2);
         CheckMethods.checkMove(-1, 2, 8, 1);
 
+        int tmp = 0;
+        for (int i = 1; i <= 64; i++) {
+            if (Main.nuts[i] == 3) {
+                tmp++;
+            }
+        }
+        if (tmp == 0) {
+            System.out.println("Pass");
+            if (Main.player == 1) {
+                Main.player = 2;
+            } else {
+                Main.player = 1;
+            }
 
+        }
     }
 }
