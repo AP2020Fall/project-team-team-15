09d44f84794426741a0d9be514fead43037e6e40
sadra mns <sadra.mansouri@yahoo.com>
2020-12-13 12:40:56 +0330

    Add Methods

--- Reversi/src/Checked.java
+++ Reversi/src/Checked.java
@@ -1,3 +1,14 @@
 public class Checked {
+    public void possibleMove(int btnIndex) {
+        //changing color
+        CheckMethods.changeColor(-1, 2, -8, 1, btnIndex);
+        CheckMethods.changeColor(-1, 2, 0, 2, btnIndex);
+        CheckMethods.changeColor(-1, 2, 8, 1, btnIndex);
+        //checking move
+        CheckMethods.checkMove(-1, 2, -8, 1);
+        CheckMethods.checkMove(-1, 2, 0, 2);
+        CheckMethods.checkMove(-1, 2, 8, 1);
+
+
 
 }
