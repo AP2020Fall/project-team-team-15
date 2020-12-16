c550cd415f80ff6fa392b1ededdc6309f02b0a1b
Matthewwwwwww <36817009+Matthewwwwwww@users.noreply.github.com>
2020-12-16 15:46:31 +0330

    Start

--- Reversi/src/Main.java
+++ Reversi/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main  {
     public static int[] nuts = new int[65];
     public static int player;
@@ -7,5 +10,20 @@ public class Main  {
     public static void main(String[] args) {
         Map map = new Map();
         map.printMap();
-    }
+        Scanner scanner = new Scanner(System.in);
+        x = 0;
+        y = 1;
+        //initialize center nuts
+        nuts[28] = 2;
+        nuts[37] = 2;
+        nuts[29] = 1;
+        nuts[36] = 1;
+
+        while (true) {
+            String s = scanner.nextLine();
+            if (s.equals("start Reversi game")) {
+                break;
+            }
+
+        }
 }
