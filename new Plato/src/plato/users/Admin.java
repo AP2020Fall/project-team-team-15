bea69cbd6483b24a8e7052f8149a03fa06dbb01e
sadra mns <sadra.mansouri@yahoo.com>
2020-12-17 09:45:00 +0330

    Add getters & setters

--- new Plato/src/plato/users/Admin.java
+++ new Plato/src/plato/users/Admin.java
@@ -4,4 +4,40 @@ public class Admin {
     String name,family,username,password,email,phone_number;
     int AdminId;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
 }
