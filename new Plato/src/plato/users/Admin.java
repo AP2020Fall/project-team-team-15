3adf5b1054297aaaecfb353333bcc33f8deb9519
sadra mns <sadra.mansouri@yahoo.com>
2020-12-17 09:46:58 +0330

    Add constructor

--- new Plato/src/plato/users/Admin.java
+++ new Plato/src/plato/users/Admin.java
@@ -40,4 +40,39 @@ public class Admin {
         return email;
     }
 
+    public Admin(String name, String family, String username, String password, String email, String phone_number, int adminId) {
+        this.name = name;
+        this.family = family;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phone_number = phone_number;
+        AdminId = adminId;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public int getAdminId() {
+        return AdminId;
+    }
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setAdminId(int adminId) {
+        AdminId = adminId;
+    }
+
 }
