3102722d856c8d3c7d5060d8b8b97d3aa696d4aa
Ramin <Raminfarahani79@yahoo.com>
2020-12-18 17:47:44 +0330

    excuse me for commiting all this class at the same time. for all the variable we define in line 6 to 10,we create a getName_variable and setName_variable method using intellij command + N ,all of them are easy to understand ,JUST excuse me for commiting all at a same time,

--- new Plato/src/plato/model/users/User.java
+++ new Plato/src/plato/model/users/User.java
@@ -1,4 +1,28 @@
 package plato.model.users;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class User {
+    String name,family,username,password,email,phone_number;
+    int userId,date_of_register,money,score;
+    Log_Of_Games log;
+    ArrayList<User> friends;
+    List friend_request;
+
+    public User(String name, String family, String username, String password, String email, String phone_number, int userId, int date_of_register, int money, int score, Log_Of_Games log, ArrayList<User> friends, List friend_request) {
+        this.name = name;
+        this.family = family;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phone_number = phone_number;
+        this.userId = userId;
+        this.date_of_register = date_of_register;
+        this.money = money;
+        this.score = score;
+        this.log = log;
+        this.friends = friends;
+        this.friend_request = friend_request;
+    }
+
 }
