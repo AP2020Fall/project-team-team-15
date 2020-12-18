784cc32dcf57ed7f6fefeee3575ec4aa83aad041
Ramin <Raminfarahani79@yahoo.com>
2020-12-18 18:42:09 +0330

    hi first we import the class we want"event", then we define variable we want in line 5 to 6,then we implement method  createEvent,in line 11 to 14 we definr set meyhod in line 11 to 14 using command + N, then we implement delete/join/leave_EVENT methods.

--- new Plato/src/plato/model/Event.java
+++ new Plato/src/plato/model/Event.java
@@ -1,4 +1,26 @@
 package plato.model;
-
+import plato.model.users.Admin;
+import plato.model.users.User;
 public class Event {
+    String name_of_game,start,end;
+    int eventId;
+    public void createEvent(){//admin ijad mikone
+
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+    public void setEnd(String end) {
+        this.end = end;
+    }
+    public void delete_Event(){//admin
+
+    }
+    public void join_Event(){//user
+
+    }
+    public void leave_Event(User user){
+
+    }
 }
