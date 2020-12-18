829ce32372c5aad27cfe04029c17c2add11cae60
Ramin <Raminfarahani79@yahoo.com>
2020-12-18 18:10:29 +0330

    excuse me for commiting all this class at the same time. for all the variable we define in line 6 to 10,we create a getName_variable and setName_variable method using intellij command + N ,all of them are easy to understand ,JUST excuse me for commiting all at a same time,

--- new Plato/src/plato/model/users/User.java
+++ new Plato/src/plato/model/users/User.java
@@ -25,4 +25,107 @@ public class User {
         this.friend_request = friend_request;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getDate_of_register() {
+        return date_of_register;
+    }
+
+    public void setDate_of_register(int date_of_register) {
+        this.date_of_register = date_of_register;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Log_Of_Games getLog() {
+        return log;
+    }
+
+    public void setLog(Log_Of_Games log) {
+        this.log = log;
+    }
+
+    public ArrayList<User> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(ArrayList<User> friends) {
+        this.friends = friends;
+    }
+
+    public List getFriend_request() {
+        return friend_request;
+    }
+
+    public void setFriend_request(List friend_request) {
+        this.friend_request = friend_request;
+    }
 }
