e5bc3b89e083332759dd7a148dd3b9a043e4592e
Ramin <Raminfarahani79@yahoo.com>
2020-12-19 20:09:38 +0330

    hi dear friend first we import all class we want,then I had create the psvm class on intellij

--- new Plato/src/plato/view/Main.java
+++ new Plato/src/plato/view/Main.java
@@ -1,5 +1,15 @@
 package plato.view;
 
+
+import plato.controller.AdminService;
+import plato.model.users.Admin;
+import plato.storage.Database;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import plato.storage.Database;
 public class Main {
     public static void main(String[] args) {
 
