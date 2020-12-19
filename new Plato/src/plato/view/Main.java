47457ebdb2ebfbe6aa04d12c8d520ec289b4539a
Ramin <Raminfarahani79@yahoo.com>
2020-12-19 20:20:13 +0330

    first in psvm I create scanner then in next line I define dataBase,after that I print welcome ,then we define a file on line 19 if the file exit we do not do anything else we do registring user i mean that first we get input user then pass,after all of these we create new userthen we do admin service and register the the admin in our system

--- new Plato/src/plato/view/Main.java
+++ new Plato/src/plato/view/Main.java
@@ -12,6 +12,26 @@ import java.util.Scanner;
 import plato.storage.Database;
 public class Main {
     public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        Database database=new Database();
 
+        System.out.println("welcome to plato");
+        File file= new File(database.PATH);
+        if (file.exists()){
+        }
+        else {
+            System.out.println("please register");
+            String username= input.nextLine();
+            String password=input.nextLine();
+            Admin admin=new Admin(username,password);
+            AdminService adminService=new AdminService();
+            System.out.println(adminService.Register(admin));
+        }
+        //   Scanner instruction=new Scanner(System.in);
+
+        //  String ins=instruction.next();
+        /*switch (ins){
+            case*/
+        //    }
     }
 }
