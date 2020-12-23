535219522255d9c732d27a7d7d073c0b8caa2120
Matthewwwwwww <36817009+Matthewwwwwww@users.noreply.github.com>
2020-12-23 15:25:42 +0330

    Update UserService.java
    Add UserService

--- new Plato/src/plato/controller/UserService.java
+++ new Plato/src/plato/controller/UserService.java
@@ -1,4 +1,28 @@
 package plato.controller;
 
+import plato.model.users.User;
+
 public class UserService {
+   public void login(){
+
+    }
+    public void logout(){
+
+    }
+    public void modifyProfile(){
+
+    }
+    public void joinEvent(){
+
+    }
+    public static void back(){
+
+    }
+    public void play(){
+
+    }
+    public void friend_request(User user){
+
+    }
+
 }
