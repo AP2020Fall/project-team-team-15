e6277e21439a13e621c5f2e93fbabada1a309588
sadra mns <sadra.mansouri@yahoo.com>
2021-01-13 16:21:54 +0330

    Add fields

---
+++ Reversi phase2/src/MapFrame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class MapFrame {
+    public static ArrayList<JButton> btns = new ArrayList<>();
+    JButton tmp;
+    JFrame frame;
+    public static JLabel whiteLbl = new JLabel();
+    public static JLabel blackLbl = new JLabel();
+    public static JLabel information = new JLabel();
+    public static int player = 1;
+}
