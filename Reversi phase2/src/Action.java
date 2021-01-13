b5e616467b13b5d0fadc902ed7024fcadbdf10b0
sadra mns <sadra.mansouri@yahoo.com>
2021-01-13 17:21:00 +0330

    Handle random button ActionListener

--- Reversi phase2/src/Action.java
+++ Reversi phase2/src/Action.java
@@ -1,9 +1,54 @@
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Action implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
+
+        if (((JButton) e.getSource()).getBackground() == Color.green) {
+            int btnIndex = 0;
+            ArrayList<Integer> grayBtn = new ArrayList<>();
+            if (MapFrame.player == 1) {
+                for (int i = 1; i <= 64; i++) {
+                    if (MapFrame.btns.get(i).getBackground() == Color.gray) {
+                        grayBtn.add(i);
+                    }
+                }
+                Random rand = new Random();
+                btnIndex = rand.nextInt(grayBtn.size());
+                MapFrame.btns.get(grayBtn.get(btnIndex)).setBackground(Color.black);
+                // برای پاک کردن موقعیت های بازی قبل
+                for (int i = 1; i <= 64; i++) {
+                    if (MapFrame.btns.get(i).getBackground() == Color.gray) {
+                        MapFrame.btns.get(i).setBackground(Color.pink);
+                    }
+                }
+                MapFrame.information.setText("player 2");
+                MapFrame.player = 2;
+            } else {
+                for (int i = 1; i <= 64; i++) {
+                    if (MapFrame.btns.get(i).getBackground() == Color.gray) {
+                        grayBtn.add(i);
+                    }
+                }
+                Random rand = new Random();
+                btnIndex = rand.nextInt(grayBtn.size());
+                MapFrame.btns.get(grayBtn.get(btnIndex)).setBackground(Color.white);
+                for (int i = 1; i <= 64; i++) {
+                    if (MapFrame.btns.get(i).getBackground() == Color.gray) {
+                        MapFrame.btns.get(i).setBackground(Color.pink);
+                    }
+                }
+
+                MapFrame.information.setText("player 1");
+                MapFrame.player = 1;
+            }
+
+        }
 
     }
 }
