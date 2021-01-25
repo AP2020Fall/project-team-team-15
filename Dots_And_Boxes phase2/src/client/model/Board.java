683b6050b1930262feb2949dc5a521e58dd67570
Matthewwwwwww <36817009+Matthewwwwwww@users.noreply.github.com>
2021-01-25 11:57:24 +0330

    Add hEdge vEdge

--- Dots_And_Boxes phase2/src/client/model/Board.java
+++ Dots_And_Boxes phase2/src/client/model/Board.java
@@ -1,4 +1,27 @@
 package client.model;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Board {
+    final static int RED = 0;
+    final static int BLUE = 1;
+    final static int BLACK = 2;
+    final static int BLANK = 3;
+
+    private int[][] hEdge;
+    private int[][] vEdge;
+    private int[][] box;
+    private int n, redScore, blueScore;
+
+    public Board(int n) {
+        hEdge = new int[n-1][n];
+        vEdge = new int[n][n-1];
+        box = new int[n-1][n-1];
+        fill(hEdge,BLANK);
+        fill(vEdge,BLANK);
+        fill(box,BLANK);
+        this.n = n;
+        redScore = blueScore = 0;
+    }
 }
