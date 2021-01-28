4a0e64a0f5433858ca507db77f477472fc2410dd
sadra mns <sadra.mansouri@yahoo.com>
2021-01-28 19:13:23 +0330

    Add UserHistory class

---
+++ Plato phase1/src/games/model/UserHistory.java
@@ -0,0 +1,34 @@
+package games.model;
+
+import java.sql.Date;
+
+public class UserHistory {
+    String game_name;
+    String username;
+    Date date;
+
+    public String getGame_name() {
+        return game_name;
+    }
+
+    public void setGame_name(String game_name) {
+        this.game_name = game_name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
