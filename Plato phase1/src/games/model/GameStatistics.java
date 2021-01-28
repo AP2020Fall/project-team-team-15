617ca189f8fcc8553c324f12edabab2aae255aac
sadra mns <sadra.mansouri@yahoo.com>
2021-01-28 19:12:02 +0330

    Add GameStatistics class

---
+++ Plato phase1/src/games/model/GameStatistics.java
@@ -0,0 +1,53 @@
+package games.model;
+
+public class GameStatistics {
+    int level;
+    int playTimes;
+    int wins;
+    int lost;
+    String game_name;
+
+
+    public String getGame_name() {
+        return game_name;
+    }
+
+    public void setGame_name(String game_name) {
+        this.game_name = game_name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getPlayTimes() {
+        return playTimes;
+    }
+
+    public void setPlayTimes(int playTimes) {
+        this.playTimes = playTimes;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLost() {
+        return lost;
+    }
+
+    public void setLost(int lost) {
+        this.lost = lost;
+    }
+
+
+
+}
