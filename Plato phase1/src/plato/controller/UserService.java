bb96f55bdd63f8c6142f1f55d532a1a18b729e11
sadra mns <sadra.mansouri@yahoo.com>
2021-01-30 21:23:11 +0330

    Create UserService class

--- Plato phase1/src/plato/controller/UserService.java
+++ Plato phase1/src/plato/controller/UserService.java
@@ -1,4 +1,34 @@
 package plato.controller;
 
+import plato.model.users.User;
+import plato.storage.Database;
+
+import java.io.IOException;
+
 public class UserService {
+    Database database = new Database();
+
+    public String register(User user) {
+        String username = user.getUsername();
+        String password = user.getPassword();
+        String name = user.getName();
+        String family = user.getFamily();
+        String email = user.getEmail();
+        String phone_number = user.getPhone_number();
+        Database database = new Database(username, password, name, family, email, phone_number, Database.PATH_USER);
+        return "you are registered...";
+    }
+
+    public void login(User user) {
+        database.setPassword(user.getPassword());
+        database.setUsername(user.getUsername());
+        database.select(Database.PATH_USER);
+    }
+
+    public void deleteAccount(User user) throws IOException {
+        String username = user.getUsername();
+        String password = user.getPassword();
+        Database database = new Database(username, password);
+        System.out.println(database.delete(Database.PATH_USER));
+    }
 }
